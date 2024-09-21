6dd7f70503841cfcb6d65add2c7086df9d68df8d
Dean Phillips <dephillips1977@gmail.com>
2024-09-21 10:48:48 -0500

    Added an example of numeric validators

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Query, Body
+from fastapi import APIRouter, Query, Body, Path
 from pydantic import BaseModel
 from typing import Optional, List
 
@@ -18,23 +18,25 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'version': version
     }
 
-@router.post('/new/{id}/comment')
+@router.post('/new/{id}/comment/{comment_id}')
 def create_comment(
     blog: BlogModel, 
     id: int, 
-    comment_id: int = Query(
+    comment_title: int = Query(
         None, 
-        title="ID of the comment", 
-        description="Some description for comment_id", 
-        alias="commentId", deprecated=True
+        title="Title of the comment", 
+        description="Some description for comment_title", 
+        alias="commentTitle", deprecated=True
     ),
     content: str = Body(..., min_length=10, max_length=50, regex="^[a-z\s]*$"),
-    v: Optional[list[str]] = Query(['1.0', '1.1', '1.2'])
+    v: Optional[list[str]] = Query(['1.0', '1.1', '1.2']),
+    comment_id: int = Path(None, gt=5, le=10)
 ):
     return {
         'blog': blog,
         'id': id,
-        'comment_id': comment_id, 
+        'comment_title': comment_title, 
         'content': content,
-        'version': v
+        'version': v,
+        'comment_id': comment_id
     }

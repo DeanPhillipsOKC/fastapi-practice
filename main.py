f313cb429aa2f6f1e4acde88d10ac9c505aef234
Dean Phillips <dephillips1977@gmail.com>
2024-09-20 22:22:06 -0500

    Added tags

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
 from typing import Optional
 
@@ -13,13 +13,13 @@ def index():
 
 # Order is important here.  If this comes after the next one it will think
 # we are trying to call the other one with "all" as the ID
-@app.get('/blog/all')
+@app.get('/blog/all', tags=['blog']) # Tags organize the swagger docs
 def get_all_blogs(page=1, page_size: Optional[int] = None):
     return {
         'message': f"All {page_size} blogs on page {page} provided"
     }
 
-@app.get('/blog/{id}/comments/{comment_id}')
+@app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, user_name: Optional[str] = None):
     return {
         'message': f"blog_id {id}, comment_id {comment_id}, valid {valid}, username {user_name}"
@@ -30,15 +30,22 @@ class BlogType(str, Enum):
     story = 'story'
     howto = 'howto'
 
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}', tags=['blog'])
 def get_blog_type(type: BlogType):
     return {
         'message': f"Blog type {type}"
     }
 
 # Example using path parameters
-@app.get('/blog/{id}')
-def get_blog(id: int): # Make sure the ID is a valid integer
-    return {
-        'message': f"Blog with id {id}"
-    }
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
+def get_blog(id: int, response: Response): # Make sure the ID is a valid integer
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {
+            'error': f"Blog {id} not found"
+        }
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {
+            'message': f"Blog with id {id}"
+        }

1eae17d1d11d3000d09112e4c90016a8ae4e89eb
Dean Phillips <dephillips1977@gmail.com>
2024-09-22 10:39:12 -0500

    Added database models and more routers

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from routers import blog_get, blog_post
+from routers import blog_get, blog_post, user, article
 from pydantic import BaseModel
 from db import models
 from db.database import engine
 
 app = FastAPI()
+app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+app.include_router(article.router)
 
 # Hello world example
 @app.get('/hello')

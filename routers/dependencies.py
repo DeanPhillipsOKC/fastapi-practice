8267f863e248077e9eb8f3e0e1e569acdc3b5283
Dean Phillips <dephillips1977@gmail.com>
2024-09-23 20:49:56 -0500

    Added simple dependency parameter example

--- routers/dependencies.py
+++ routers/dependencies.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter, Depends
 from fastapi.requests import Request
+from typing import Optional
 
 router = APIRouter(
     prefix="/dependencies",
     tags=["dependencies"]
 )
 
-def convert_headers(request: Request):
+def convert_headers(request: Request, seperator: str = "--"):
     out_headers = []
     for key, value in request.headers.items():
-        out_headers.append(f"{key} -- {value}")
+        out_headers.append(f"{key} {seperator} {value}")
     return out_headers
 
 @router.get('')
-def get_items(headers = Depends(convert_headers)):
+def get_items(seperator: str = "--", headers = Depends(convert_headers)):
     return {
         "items": ['a', 'b', 'c'],
         "headers": headers
